4c805f4b12ed6fa6ced6d484f306ad159cdab0dc
Jakub Dąbek <jakub.dabek@gmail.com>
2022-09-20 14:56:42 +0200

    Add shorten and open routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-#[macro_use] extern crate rocket;
+#![deny(rust_2018_idioms)]
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use std::collections::{hash_map::Entry, HashMap};
+
+use rand::prelude::*;
+use rocket::{http::uri::Absolute, response::Redirect, tokio::sync::Mutex, State};
+
+#[macro_use]
+extern crate rocket;
+
+// Async Mutex to not block the executor during requests,
+// although locks should be held for short enough that it wouldn't matter.
+type UriStore = Mutex<HashMap<u64, Absolute<'static>>>;
+
+#[post("/shorten", data = "<url>")]
+async fn shorten(url: String, uri_store: &State<UriStore>) -> Result<String, String> {
+    let url = Absolute::parse_owned(url).map_err(|e| e.to_string())?;
+    let key = {
+        let mut uri_store = uri_store.lock().await;
+        let rng = &mut thread_rng();
+        loop {
+            let key = rng.gen();
+            match uri_store.entry(key) {
+                Entry::Occupied(_) => continue,
+                Entry::Vacant(v) => {
+                    v.insert(url);
+                    break key;
+                }
+            }
+        }
+    };
+
+    Ok(format!("http://localhost:8000/open/{key}"))
+}
+
+#[get("/open/<id>")]
+async fn open(id: u64, uri_store: &State<UriStore>) -> Result<Redirect, &'static str> {
+    let uri = uri_store
+        .lock()
+        .await
+        .get(&id)
+        .ok_or("Given link doesn't exist")?
+        .clone();
+    Ok(Redirect::to(uri))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![shorten, open])
+        .manage(UriStore::default())
 }
